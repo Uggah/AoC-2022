e742ace3706575aa77ed46d1bdbaf33e2fb80dbf
Uggah <luccagreschner@gmail.com>
2022-12-05 23:11:01 +0100

    Improve implementation

--- 5/5.go
+++ 5/5.go
@@ -18,41 +18,11 @@ func main() {
 	run_second()
 }
 
-func get_stack_count(path string) int {
-	file, err := os.Open(path)
-	check(err)
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	line := scanner.Text()
-
-	return (len(line) + 1) / 4
-}
-
-func log_stack(stack []byte) string {
-	returnString := ""
-
-	for i := range stack {
-		returnString = returnString + string(stack[i])
-	}
-
-	return returnString
-}
-
-func log_stacks(stacks [][]byte) string {
-	returnString := "\n"
-
-	for i := range stacks {
-		returnString = returnString + log_stack(stacks[i]) + "\n"
-	}
-
-	return returnString
-}
-
 func run_first() {
 	path := "./input/5.txt"
 
+	stacks := read_stacks(path)
+
 	file, err := os.Open(path)
 	check(err)
 	defer file.Close()
@@ -60,62 +30,18 @@ func run_first() {
 	scanner := bufio.NewScanner(file)
 	check(err)
 
-	stack_count := get_stack_count(path)
-
-	fmt.Print("Got stack count:", stack_count)
-
-	stacks := make([][]byte, stack_count)
-
-	for i := range stacks {
-		stacks[i] = make([]byte, 0)
-	}
-
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		if line == "" {
-			break
+		if len(line) == 0 || line[0] != 'm' {
+			continue
 		}
 
-		for i := 0; i < stack_count*4; i += 4 {
-			stack := stacks[i/4]
+		amount, from, to := read_instruction(line)
 
-			letter := line[i+1]
+		fmt.Printf("Instruction: Move %d from %d to %d. \n", amount, from, to)
 
-			if letter >= 65 && letter <= 90 {
-				stacks[i/4] = append([]byte{letter}, stack...)
-				fmt.Println("Got: ", string(letter))
-			}
-
-		}
-
-	}
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		amount := int64(0)
-		offset := 0
-
-		if line[6] == ' ' {
-			amount, err = strconv.ParseInt(string(line[5]), 10, 64)
-			check(err)
-		} else {
-			amount, err = strconv.ParseInt(string(line[5:7]), 10, 64)
-			check(err)
-
-			offset = 1
-		}
-
-		from, err := strconv.ParseInt(string(line[12+offset]), 10, 64)
-		check(err)
-
-		to, err := strconv.ParseInt(string(line[17+offset]), 10, 64)
-		check(err)
-
-		fmt.Println("Got amount, from, to:", amount, from, to)
-
-		fmt.Println("Before:", log_stacks(stacks))
+		fmt.Println("Stacks before execution:", log_stacks(stacks))
 
 		from_stack := stacks[from-1]
 		from_stack_size := len(from_stack)
@@ -125,22 +51,7 @@ func run_first() {
 			stacks[from-1] = stacks[from-1][:len(stacks[from-1])-1]
 		}
 
-		/*
-			from_elements := reverse(from_stack[(from_stack_size - amount):from_stack_size])
-
-			fmt.Println("Elements to move:", log_stack(from_elements))
-			fmt.Println()
-
-			stacks[from-1] = stacks[from-1][0:(from_stack_size - amount)]
-
-			for i := range from_elements {
-				element := from_elements[i]
-
-				stacks[to-1] = append(stacks[to-1], element)
-			}
-		*/
-
-		fmt.Println("After:", log_stacks(stacks))
+		fmt.Println("Stacks after execution:", log_stacks(stacks))
 	}
 
 	fmt.Println("First task:")
@@ -154,6 +65,8 @@ func run_first() {
 func run_second() {
 	path := "./input/5.txt"
 
+	stacks := read_stacks(path)
+
 	file, err := os.Open(path)
 	check(err)
 	defer file.Close()
@@ -161,9 +74,62 @@ func run_second() {
 	scanner := bufio.NewScanner(file)
 	check(err)
 
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if len(line) == 0 || line[0] != 'm' {
+			continue
+		}
+
+		amount, from, to := read_instruction(line)
+
+		fmt.Printf("Instruction: Move %d from %d to %d. \n", amount, from, to)
+
+		fmt.Println("Stacks before execution:", log_stacks(stacks))
+
+		from_stack := stacks[from-1]
+		from_stack_size := int64(len(from_stack))
+
+		from_elements := from_stack[(from_stack_size - amount):from_stack_size]
+
+		stacks[from-1] = stacks[from-1][0:(from_stack_size - amount)]
+
+		stacks[to-1] = append(stacks[to-1], from_elements...)
+
+		fmt.Println("Stacks after execution:", log_stacks(stacks))
+	}
+
+	fmt.Println("Second task:")
+
+	for i := range stacks {
+		stack := stacks[i]
+		fmt.Print(string(stack[len(stack)-1]))
+	}
+
+}
+
+// HELPER FUNCTIONS
+
+func get_stack_count(path string) int {
+	file, err := os.Open(path)
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	line := scanner.Text()
+
+	return (len(line) + 1) / 4
+}
+
+func read_stacks(path string) [][]byte {
 	stack_count := get_stack_count(path)
 
-	fmt.Print("Got stack count:", stack_count)
+	file, err := os.Open(path)
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
 
 	stacks := make([][]byte, stack_count)
 
@@ -185,59 +151,57 @@ func run_second() {
 
 			if letter >= 65 && letter <= 90 {
 				stacks[i/4] = append([]byte{letter}, stack...)
-				fmt.Println("Got: ", string(letter))
 			}
 
 		}
 
 	}
 
-	for scanner.Scan() {
-		line := scanner.Text()
+	return stacks
+}
 
-		amount := int64(0)
-		offset := 0
+func read_instruction(line string) (int64, int64, int64) {
+	var err error
+	var amount int64
+	var offset int
 
-		if line[6] == ' ' {
-			amount, err = strconv.ParseInt(string(line[5]), 10, 64)
-			check(err)
-		} else {
-			amount, err = strconv.ParseInt(string(line[5:7]), 10, 64)
-			check(err)
-
-			offset = 1
-		}
-
-		from, err := strconv.ParseInt(string(line[12+offset]), 10, 64)
+	if line[6] == ' ' {
+		amount, err = strconv.ParseInt(string(line[5]), 10, 64)
 		check(err)
 
-		to, err := strconv.ParseInt(string(line[17+offset]), 10, 64)
+		offset = 0
+	} else {
+		amount, err = strconv.ParseInt(string(line[5:7]), 10, 64)
 		check(err)
 
-		fmt.Println("Got amount, from, to:", amount, from, to)
-
-		fmt.Println("Before:", log_stacks(stacks))
-
-		from_stack := stacks[from-1]
-		from_stack_size := int64(len(from_stack))
-
-		from_elements := from_stack[(from_stack_size - amount):from_stack_size]
-
-		fmt.Println("Elements to move:", log_stack(from_elements))
-		fmt.Println()
-
-		stacks[from-1] = stacks[from-1][0:(from_stack_size - amount)]
-
-		stacks[to-1] = append(stacks[to-1], from_elements...)
-
-		fmt.Println("After:", log_stacks(stacks))
+		offset = 1
 	}
 
-	fmt.Println("Second task:")
+	from, err := strconv.ParseInt(string(line[12+offset]), 10, 64)
+	check(err)
+
+	to, err := strconv.ParseInt(string(line[17+offset]), 10, 64)
+	check(err)
+
+	return amount, from, to
+}
+
+func log_stack(stack []byte) string {
+	returnString := ""
+
+	for i := range stack {
+		returnString = fmt.Sprint(returnString, "[", string(stack[i]), "]")
+	}
+
+	return returnString
+}
+
+func log_stacks(stacks [][]byte) string {
+	returnString := "\n"
 
 	for i := range stacks {
-		stack := stacks[i]
-		fmt.Print(string(stack[len(stack)-1]))
+		returnString = fmt.Sprint(returnString, i, ": ", log_stack(stacks[i]), "\n")
 	}
 
+	return returnString
 }
